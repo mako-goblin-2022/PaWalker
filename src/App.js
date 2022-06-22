0dcd6a4fb03a0d1c40f9fd946c651ecba41f61e9
Rodrigo Augusto Reis <rodrigoreis@rodrigos-mbp.lan>
2022-06-22 20:42:50 +1200

    Add db and some components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header'
 import Product from './components/Products'
 import Footer from './components/Footer'
+import SignIn from './components/SignIn';
 
 
 function App() {
   return (
     <div >
       <Header />
+      <SignIn />
       <Product />
       <Footer />
     </div>
