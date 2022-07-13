35cda26b4337a83c5324320cf9641cf61ca6d5e2
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-14 11:14:26 +1200

    getUserById function work

--- server/db/users.db.js
+++ server/db/users.db.js
@@ -1,9 +1,15 @@
+const config = require('./knexfile')
 const connection = require('./connection')
 
 function getUsers(db = connection) {
   return db('users').select()
 }
 
+function getUserById(id, db = connection) {
+  return db('users').select().where('auth0_id', id).first()
+}
+
 module.exports = {
   getUsers,
+  getUserById,
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,14 +6,21 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   db.getUsers()
-    .then(results => {
-      res.json({ users: results.map(user => user.name) })
+    .then((results) => {
+      res.json({ users: results.map((user) => user.name) })
       return null
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err)
       res.status(500).json({ message: 'Somthing went wrong' })
     })
+})
+
+router.get('/:id', (req, res) => {
+  const userId = req.params.id
+  db.getUserById(userId)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
 })
 
 module.exports = router
