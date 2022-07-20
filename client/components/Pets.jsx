4408efee457ef848e5c1c529c88eabb187d35bb5
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-07-20 20:37:41 +1200

    Complete pet and walkers profile view button

--- client/components/Pets.jsx
+++ client/components/Pets.jsx
@@ -6,24 +6,20 @@ import PetByLocation from './PetByLocation'
 import Header from './Header'
 // import { fetchAllUsers } from '../features/users/usersSlice'
 import Users from './Users'
-
+import { Link, useNavigate } from 'react-router-dom'
 
 function Pets() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  
   const petsDisplay = useSelector((state) => {
     // console.log(state)
     return state.pet
   })
 
-  
-
   useEffect(() => {
     dispatch(fetchAllPets())
   }, [dispatch])
-
-  
 
   const addHistory = async (userId) => {
     // await setHistoryList(historyList.push(userId))
@@ -35,61 +31,42 @@ function Pets() {
   //   dispatch(addToHistoryPet(userId))
   // }
 
-
-
   return (
     <div>
       <div>
         <Header />
       </div>
       <div className='w-1/3 mb-12 text-center m-auto'>
-      <PetByLocation />
-     
-        </div>
+        <PetByLocation />
+      </div>
       <main className='max-w-6xl mx-auto text-center mb-20 grid grid-cols-3 gap-5'>
-      
-      
         {petsDisplay.pets.map((pets, i) => {
           return (
-            <div key={i} className="px-2 py-2 rounded-lg mx-auto">
+            <div key={i} className='px-2 py-2 rounded-lg mx-auto'>
               <img
-                className="rounded-lg object-cover h-48 w-64"
+                className='rounded-lg object-cover h-48 w-64'
                 src={pets.img}
                 alt={pets.name}
               />
-              <h3 className="text-blue-500 font-semibold">{pets.name}</h3>
-              <h3 className="">{pets.location}</h3>
+              <h3 className='text-blue-500 font-semibold'>{pets.name}</h3>
+              <h3 className=''>{pets.location}</h3>
 
-
+              <Link to={`/pets/${pets.id}`}>
+                <button className='w-20 cursor-pointer uppercase h-9 bg-yellow-500 rounded-3xl text-white text-xs font-semibold  shadow-xl hover:bg-yellow-600 hover:text-black hover:translate-y-px'>
+                  Owner Profile
+                </button>
+              </Link>
               <button
-                className="w-20 cursor-pointer uppercase h-9 bg-yellow-500 rounded-3xl text-white text-xs font-semibold  shadow-xl hover:bg-yellow-600 hover:text-black hover:translate-y-px"
-                onClick={() => addHistory(pets.id)}
-              >
-                Owner Profile
-              </button>
-              <button
-                className="w-20 cursor-pointer uppercase h-9 bg-yellow-500 rounded-3xl text-white text-xs font-semibold  shadow-xl hover:bg-yellow-600 hover:text-black hover:translate-y-px"
+                className='w-20 cursor-pointer uppercase h-9 bg-yellow-500 rounded-3xl text-white text-xs font-semibold  shadow-xl hover:bg-yellow-600 hover:text-black hover:translate-y-px'
                 onClick={() => addHistory(pets.id)}
               >
                 Add History
               </button>
-              <p
-                className={
-                  petsDisplay.history.includes(pets.id) ? '' : 'invisible'
-                }
-              >
+              <p className={petsDisplay.history.includes(pets.id) ? '' : 'invisible'}>
                 {pets.name} has been added to your history.
               </p>
-              <div>
-                
-              </div>
-              <p
-                className={
-                  petsDisplay.history.includes(pets.id) ? '' : 'invisible'
-                }
-              >
-             
-              </p>
+              <div></div>
+              <p className={petsDisplay.history.includes(pets.id) ? '' : 'invisible'}></p>
             </div>
           )
         })}
--- client/components/Walkers.jsx
+++ client/components/Walkers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAllUsers } from '../features/users/usersSlice'
 import Header from './Header'
+import { Link } from 'react-router-dom'
 
 function Walkers() {
   const walkersDisplay = useSelector((state) => {
@@ -23,17 +24,18 @@ function Walkers() {
       <div>
         <Header />
       </div>
-      <main className="max-w-3xl mx-auto text-center mb-20 grid grid-cols-2 gap-4">
+      <main className='max-w-3xl mx-auto text-center mb-20 grid grid-cols-2 gap-4'>
         {walker.map((walkers, i) => {
           return (
-            <div key={i} className="px-2 py-2 rounded-lg mx-auto">
+            <div key={i} className='px-2 py-2 rounded-lg mx-auto'>
               <img
-                className="rounded-lg object-cover h-48 w-64 shadow-xl"
+                className='rounded-lg object-cover h-48 w-64 shadow-xl'
                 src={walkers.img}
                 alt={walkers.name}
               />
-              <h3 className="text-blue-600 font-bold">{walkers.name}</h3>
-              <h3 className="font-semibold">{walkers.location}</h3>
+              <h3 className='text-blue-600 font-bold'>{walkers.name}</h3>
+              <h3 className='font-semibold'>{walkers.location}</h3>
+              <Link to={`/users/${walkers.auth0_id}`}>View Profile</Link>
             </div>
           )
         })}
